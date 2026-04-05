b63f4f3ec7a8e8d3534bb4f86640784137369a64
4virus7 <sarfarazhussain8122@gmail.com>
2026-04-05 11:31:51 +0530

    fast fail

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const dbConnect = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Attempting to connect to MongoDB. MONGO_URI is ${process.env.MONGO_URI ? 'defined' : 'UNDEFINED'}`);
+        await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000 // Fail after 5 seconds instead of hanging
+        })
         console.log('Mongo DB Connected Success')
     } catch(error){
-        console.log(`Error Connecting with Mongo: ${error.message}`)
+        console.error(`Error Connecting with Mongo: ${error.message}`)
         process.exit(1)
     }
 }
